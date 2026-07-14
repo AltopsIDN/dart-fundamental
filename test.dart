e507961794e6cc8b478082d290c1a20f727bb89a
AltopsIDN <altopsidn@gmail.com>
2026-07-14 10:34:39 +0700

    first commit

---
+++ test.dart
@@ -0,0 +1,15 @@
+// single comment
+
+/// aturan dari documentation comment
+/// 1. ditandai dengan ///
+/// 2. untuk memanggil nama dunction harus menggunakan []
+/// [age]
+void main(List<String> args) {
+  print("Hello World");
+
+  print('object');
+}
+
+int age() {
+  return 17;
+}
