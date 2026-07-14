3938b3319e9743c709d9e920889dd9430c8305fc
AltopsIDN <altopsidn@gmail.com>
2026-07-14 11:15:07 +0700

    Add missing newlines and improve formatting in const.dart

--- const.dart
+++ const.dart
@@ -5,5 +5,17 @@
 const num pi = 3.14;
 
 void main(List<String> args) {
+  var radius = 10; // bisa diubah (muttable) karna ada var
+
+  print('LUAS lingkaran dengan radius $radius = ${luasLingkaran(radius)}');
+
+  final firstName = "altop";
+  final lastname = "joestar";
+
+  // lastname = "hamid";
   
+  print('Namaku $firstName $lastname');
+
 }
+
+num luasLingkaran(num radius) => pi * radius * radius;
